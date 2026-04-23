96aea0d5fb0460cfa0247d94c1fbe8e1928d55f5
NguyenThuyDiep-25021671 <dipdipomom@gmail.com>
2026-04-23 17:48:28 +0700

    Cập nhật giao diện của Diệp

---
+++ src/main/java/com/auction/client/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.auction.client.controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginController {
+    @FXML
+    public void Open_the_home_page(ActionEvent event){
+        System.out.println("Chưa có đâu ahihi 😒");
+    }
+    @FXML
+    public void Welcome_back(ActionEvent event){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/auction/client/view/WelcomeView.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.auction {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.desktop;
+
+    opens com.auction.client.controller to javafx.fxml;
+    opens com.auction to javafx.fxml;
+
+    exports com.auction;
+    exports com.auction.client.controller;
+}
