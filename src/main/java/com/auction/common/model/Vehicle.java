2651895408b8650637a96520809dab66d55fa85a
Thuy Duong <nguyentam06197205@gmail.com>
2026-04-23 18:56:21 +0700

    Art.java

---
+++ src/main/java/com/auction/common/model/Vehicle.java
@@ -0,0 +1,8 @@
+package com.auction.common.model;
+
+class Vehicle extends Items{
+    public Vehicle (int id,String producer,int price,String show,String name){
+
+        super(id,producer,price,show,name);
+    }
+}
