a1d526a81762f7ee1812e333529c367ca499278e
PhanThuyDuong25021701TDTT <nguyentam06197205@gmail.com>
2026-04-24 08:18:01 +0700

    Delete src/main/java/com/Vehicle.java

--- src/main/java/com/Vehicle.java
+++
@@ -1,8 +0,0 @@
-package com.auction.common.model;
-
-class Vehicle extends Items{
-    public Vehicle (int id,String producer,int price,String show,String name){
-
-        super(id,producer,price,show,name);
-    }
-}
