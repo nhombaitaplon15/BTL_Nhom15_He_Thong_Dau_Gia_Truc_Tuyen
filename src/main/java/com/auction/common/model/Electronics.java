e7dd286d986d386304812e76c576ba7ebbd250bf
Thuy Duong <nguyentam06197205@gmail.com>
2026-04-26 08:48:28 +0700

    update class electronics

--- src/main/java/com/auction/common/model/Electronics.java
+++ src/main/java/com/auction/common/model/Electronics.java
@@ -1,9 +1,9 @@
 package com.auction.common.model;
 
 public class Electronics extends Items implements java.io.Serializable{
-    protected int date;
+    protected String date;
     protected int warrantyExpiryDate;
-    public Electronics (String name,int id,String producer,int price, String show,String imgitem, int date, int warrantyExpiryDate ){
+    public Electronics (String name,int id,String producer,int price, String show,String imgitem, String date, int warrantyExpiryDate ){
         super(id,producer,price,show,name, imgitem);
         this.date= date;
         this.warrantyExpiryDate= warrantyExpiryDate;
