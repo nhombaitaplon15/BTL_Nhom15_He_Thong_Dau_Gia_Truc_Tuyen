b07442f32e8e0cfaaeb7b230a1e27f937fb80478
Thuy Duong <nguyentam06197205@gmail.com>
2026-04-26 08:32:17 +0700

    update class Items

--- src/main/java/com/auction/common/model/Items.java
+++ src/main/java/com/auction/common/model/Items.java
@@ -2,11 +2,45 @@ package com.auction.common.model;
 public abstract class Items extends Entity implements java.io.Serializable{
     protected String producer;
     protected int price;
-    protected String show;
-    public Items (int id,String producer,int price,String show,String name){
+    protected String show;   //mô tả của sản phẩm
+    protected String name;
+    protected  String imgitem;
+    public Items (int id,String producer,int price,String show,String name, String imgitem){
         this.producer=producer;
         this.price=price;
         this.show=show;
+        this.name= name;
+        this.imgitem= imgitem;
         super(id);
+    }
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+    public String getProducer() {
+        return producer;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setShow(String show) {
+        this.show = show;
+    }
+    public String getShow(){
+        return show;
+    }
+    public void setimgitem(String imgitem) {
+        this.imgitem = imgitem;
+    }
+    public String getimgitem(){
+        return imgitem;
     }
 }
