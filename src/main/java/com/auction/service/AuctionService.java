6cba2e068c0947e1a9e7f879089e9da8d6c36095
Thuy Duong <nguyentam06197205@gmail.com>
2026-04-27 10:18:36 +0700

    các hàm add, update, delete,thiết lập giá khởi điểm + mô tả , hàm chuyển trạng thái

---
+++ src/main/java/com/auction/service/AuctionService.java
@@ -0,0 +1,62 @@
+package com.auction.service;
+import com.auction.common.model.Items;
+import com.auction.common.model.Auction;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuctionService {
+    private Map<Integer, Items> itemsList = new HashMap<>();                 // list sản phẩm có id và những thứ chứ trong Items
+    private Map<Integer, Auction> auctionList = new HashMap<>();             // đấu giá có id của buổi đấu giá và has-a với class Auction
+    public void addItem (Items item){                                        //hàm thêm sản phẩm
+        if(item == null){                                                    // phải tạo đối tượng cụ thể rồi addItem
+            System.out.println("Item không hợp lệ !");
+            return;
+        }
+        itemsList.put(item.getId(), item);                                  // thêm id và sp vào danh sách
+        System.out.println("Đã thêm sản phẩm: " + item.getName());
+    }
+    public void updateItem(int id, String producer, int price, String show,String name, String imgitem){ // hàm update sản phẩm
+        Items item = itemsList.get(id);                                                                  // duyệt id để update, sản phẩm mà không tồn tại thì không update được
+        if (item == null)
+            return;
+        item.setProducer(producer);
+        item.setPrice(price);
+        item.setShow(show);
+        item.setName(name);
+        item.setimgitem(imgitem);
+        System.out.println("Đã cập nhật sản phẩm!");
+    }
+    public void deleteItem(int id){                                          //xóa sản phẩm cũng duyệt id và xóa id
+        itemsList.remove(id);
+        System.out.println(" Đã xóa ID sản phẩm: "+ id);
+    }
+    public void setStartingPrice(int id, int price){                        // hàm set giá này thì set giá có ở class Items nên chỉ cần gọi hàm ở class Items
+        Items item = itemsList.get(id);
+        if (item == null){
+            System.out.println("Không tìm thấy sản phẩm !");
+            return;
+        }
+        item.setPrice(price);
+        System.out.println("Đã thiết lập giá khởi điểm là: "+ price);
+    }
+    public void setDescription(int id,String show){                       // hàm set mô tả cũng như hàm set giá
+        Items item = itemsList.get(id);
+        if ( item ==null){
+            System.out.println("Không tìm thấy sản phẩm!");
+            return;
+        }
+        item.setShow(show);
+        System.out.println("Đã cập nhật mô tả sản phẩm!" + show);
+    }
+    public void setAuctionSchedule(int auctionId, LocalDateTime startTime) { // chuyển trạng thái
+        Auction auction = auctionList.get(auctionId);                        // duyệt id của Auction
+        if (auction == null) {                                              // không có buổi đấu giá thì in và thoát hàm
+            System.out.println("Không có phiên đấu giá này !");
+            return;
+        }
+        auction.setAuctionSchedule(startTime);
+        System.out.println("Trạng thái hiện tại: " + auction.getStatus()); // có thì in trạng thái theo quy định hàm ở class Auction rồi
+    }
+}
