44c18c3a2dd93cc686fb884e8a37e14049d32bad
Thuy Duong <nguyentam06197205@gmail.com>
2026-04-27 10:17:04 +0700

    em tạo 1 phiên đấu giá

--- src/main/java/com/auction/common/model/Auction.java
+++ src/main/java/com/auction/common/model/Auction.java
@@ -1,16 +1,27 @@
 package com.auction.common.model;
 
+import java.time.LocalDateTime;
+
 public class Auction {
-    private Items item;     // phiên đấu giá có chứa sản phẩm của class items
-    private int duration;    // thời gian của 1 phiên đấu giá
-    public Auction(Items item, int duration){
-        this.item= item;
-        this.duration= duration;
+    private Items item;                                    // phiên đấu giá có chứa sản phẩm của class items
+    private LocalDateTime startTime;                       //thời gian bắt đầu phiên đấu giá
+    private String status;                                 // trang thái của phiên đấu giá
+    public Auction (Items item){
+        this.item = item;
     }
     public Items getItem(){
         return item;
     }
-    public int getDuration(){
-        return duration;
+    public String getStatus(){                            // hàm viết quy định về trạng thái phiên đấu giá
+        if(startTime == null){                            //chưa set thời gian ban đầu thì chỉ mở thôi
+            return "OPEN";
+        }
+        if(LocalDateTime.now().isAfter(startTime)){
+            return "RUNNING";
+        }
+        return "OPEN";                                    //các trường hợp khác chỉ đang mở thôi
+    }
+    public void setAuctionSchedule(LocalDateTime startTime){// hàm lấy thời điểm bắt đầu đấu giá
+        this.startTime= startTime;
     }
 }
