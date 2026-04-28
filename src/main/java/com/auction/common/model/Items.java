3e438bc7100d415e402d4089ef1ad8e87b37f760
Thuy Duong <nguyentam06197205@gmail.com>
2026-04-28 02:54:37 +0000

    sửa lại Items

--- src/main/java/com/auction/common/model/Items.java
+++ src/main/java/com/auction/common/model/Items.java
@@ -1,54 +1,47 @@
 package com.auction.common.model;
+
 public abstract class Items extends Entity implements java.io.Serializable{
     protected String producer;
     protected int startPrice;
-    protected int currentPrice;
-    protected String show;   //mô tả của sản phẩm
+    protected String description;   //mô tả của sản phẩm
     protected String name;
-    protected  String imgItem;
-    public Items (int id,String producer,int startPrice, int currentPrice,String show,String name, String imgItem){
+    protected String imgItem;
+    public Items (int id,String producer,int startPrice, String description, String name, String imgItem){
         super(id);
-        this.producer=producer;
-        this.startPrice=startPrice;
-        this.currentPrice= currentPrice;
-        this.show=show;
-        this.name= name;
-        this.imgItem= imgItem;
-    }
-    public void setProducer(String producer) {
         this.producer = producer;
+        this.startPrice = startPrice;
+        this.description = description;
+        this.name = name;
+        this.imgItem = imgItem;
     }
     public String getProducer() {
         return producer;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setProducer(String producer) {
+        this.producer = producer;
     }
     public String getName(){
         return name;
     }
-    public void setStartPrice(int startPrice) {
-        this.startPrice = startPrice;
+    public void setName(String name) {
+        this.name = name;
     }
     public int getStartPrice() {
         return startPrice;
     }
-    public void setCurrentPrice(int currentPrice) {
-        this.currentPrice = currentPrice;
+    public void setStartPrice(int startPrice) {
+        this.startPrice = startPrice;
     }
-    public int getCurrentPrice() {
-        return currentPrice;
+    public String getDescription(){
+        return description;
     }
-    public void setShow(String show) {
-        this.show = show;
+    public void setDescription(String description) {
+        this.description = description;
     }
-    public String getShow(){
-        return show;
-    }
-    public void setimgItem(String imgItem) {
-        this.imgItem = imgItem;
-    }
-    public String getimgItem(){
+    public String getImgItem(){
         return imgItem;
+    }
+    public void setImgItem(String imgItem) {
+        this.imgItem = imgItem;
     }
 }
