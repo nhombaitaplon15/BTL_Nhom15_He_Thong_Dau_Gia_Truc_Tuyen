b020182cb265f2302da291c18025432d45bb7958
Thuy Duong <nguyentam06197205@gmail.com>
2026-04-28 06:10:41 +0000

    sửa lại

--- src/main/java/com/auction/service/ItemService.java
+++ src/main/java/com/auction/service/ItemService.java
@@ -7,48 +7,61 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ItemService {
-    private Map<Integer, Items> itemsList = new HashMap<>();                 // list sản phẩm có id và những thứ chứ trong Items
-    public void addItem (Items item){                                        //hàm thêm sản phẩm
-        if(item == null){                                                    // phải tạo đối tượng cụ thể rồi addItem
-            System.out.println("Item không hợp lệ !");
-            return;
-        }
-        itemsList.put(item.getId(), item);                                  // thêm id và sp vào danh sách
-        System.out.println("Đã thêm sản phẩm: " + item.getName());
+private Map<Integer, Items> itemsList = new HashMap<>();                 // list sản phẩm có id và những thứ chứ trong Items
+    public Items findItem(int id) {                                         // hàm tìm kiếm sản phẩm trong list bằng ID sản phẩm
+        return itemsList.get(id);
     }
-    public void updateItem(int id,String producer, String show,String name, String imgItem) { // hàm update sản phẩm
-        Items item = itemsList.get(id);                                                                  // duyệt id để update, sản phẩm mà không tồn tại thì không update được
-        if (item == null){
-            System.out.println("Item không hợp lệ !");
-            return;
+    public boolean addItem(Items item) throws Exception {                   // hàm thêm sản phẩm
+        try {
+            if (item == null) {                                             // nếu không có sản phẩm
+                throw new Exception("Sản phẩm không được để trống !");
+            }
+            if (item.getName() == null) {                                   // sản phẩm không có tên throw
+                throw new Exception("Tên sản phẩm không được để trống!");
+            }
+            if (itemsList.containsKey(item.getId())) {                      // ID sản phẩm có thì không thể thêm thành sản phẩm mới được
+                throw new Exception("ID sản phẩm đã tồn tại !");
+            }
+            itemsList.put(item.getId(), item);                              // nếu không lỗi thì cho sản phẩm vào trong danh sách
+            System.out.println(" Đã thêm sản phẩm: " + item.getName());
+            return true;
+        } catch(Exception e) {                                              // sai thì bắt ngoại lệ để in lỗi
+            System.out.println("[Lỗi] :" + e.getMessage());
+            throw e;
         }
-        item.setName(name);
-        item.setProducer(producer);
-        item.setShow(show);
-        item.setimgItem(imgItem);
-        System.out.println("Đã cập nhật sản phẩm!");
     }
-    public void deleteItem(int id){                                          //xóa sản phẩm cũng duyệt id và xóa id
-        itemsList.remove(id);
-        System.out.println(" Đã xóa ID sản phẩm: "+ id);
-    }
-    public void setStartingPrice(int id, int price){                        // hàm set giá này thì set giá có ở class Items nên chỉ cần gọi hàm ở class Items
-        Items item = itemsList.get(id);
-        if (item == null){
-            System.out.println("Không tìm thấy sản phẩm !");
-            return;
+    public boolean updateItem(Items item) throws Exception {                 //hàm cập nhật sản phẩm
+        try {
+            if (item == null) {
+                throw new Exception(" Sản phẩm không được để trống!");
+            }
+            if (!itemsList.containsKey(item.getId())) {                      // ID sản phẩm mà không có trong itemList thì không update được
+                throw new Exception("ID sản phẩm không tồn tại!");
+            }
+            Items oldItem = itemsList.get(item.getId());                     // đây là sản phẩm cần được update
+            oldItem.setName(item.getName());                                 // update name
+            oldItem.setProducer(item.getProducer());
+            oldItem.setStartPrice(item.getStartPrice());
+            oldItem.setDescription(item.getDescription());
+            oldItem.setImgItem(item.getImgItem());
+            System.out.println("Cập nhật sản phẩm thành công");
+            return true;
+        } catch (Exception e) {
+            System.out.println("[Lỗi] :" + e.getMessage());
+            throw e;
         }
-        item.setStartPrice(price);                                        // khi set giá khởi điểm thì giá khởi điểm sẽ là giá hiện tại
-        item.setCurrentPrice(price);
-        System.out.println("Đã thiết lập giá khởi điểm là: "+ price);
     }
-    public void setDescription(int id,String show){                       // hàm set mô tả cũng như hàm set giá
-        Items item = itemsList.get(id);
-        if ( item ==null){
-            System.out.println("Không tìm thấy sản phẩm!");
-            return;
+    public boolean deleteItem(int itemId) throws Exception{                  // hàm xóa sản phẩm
+        try {
+            if (!itemsList.containsKey(itemId)) {                            // nếu trong ItemList mà không có ID sản phẩm thì không xóa sp
+                throw new Exception("ID sản phẩm không tồn tại để xóa!");
+            }
+            itemsList.remove(itemId);
+            System.out.println("Xóa sản phẩm thành công");
+            return true;
+        } catch (Exception e) {
+            System.out.println("[Lỗi] :" + e.getMessage());
+            throw e;
         }
-        item.setShow(show);
-        System.out.println("Đã cập nhật mô tả sản phẩm!" + show);
     }
 }
--- src/main/java/com/auction/service/ManagerService.java
+++ src/main/java/com/auction/service/ManagerService.java
@@ -1,5 +1,5 @@
 package com.auction.service;
-
+import com.auction.common.model.Items;
 import com.auction.common.model.Auction;
 
 import java.time.LocalDateTime;
@@ -7,14 +7,60 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ManagerService {
-    private Map<Integer, Auction> auctionList = new HashMap<>();             // đấu giá có id của buổi đấu giá và has-a với class Auction
-    public void setAuctionSchedule(int auctionId, LocalDateTime startTime) { // chuyển trạng thái
-        Auction auction = auctionList.get(auctionId);                        // duyệt id của Auction
-        if (auction == null) {                                              // không có buổi đấu giá thì in và thoát hàm
-            System.out.println("Không có phiên đấu giá này !");
-            return;
+    private ItemService itemService;                                            // gọi 1 kho quản lí sản phẩm
+    private Map<Integer, Auction> auctionList = new HashMap<>();                // đấu giá có id của buổi đấu giá và has-a với class Auction
+    public ManagerService(ItemService itemService) {
+        this.itemService = itemService;
+    }
+    public void setupStartPrice(int itemId, int newPrice) throws Exception{     // hàm thiết lập giá khởi điểm
+        try{
+            Items item = itemService.findItem(itemId);                          // gọi 1 sản phẩm trong kho theo ID
+            if( item == null){
+                throw new Exception(" Không tìm thấy sản phẩm!");
+            }
+            if (newPrice <= 0) {
+                throw new Exception("Giá khởi điểm phải lớn hơn 0!");
+            }
+            item.setStartPrice(newPrice);                                       // nếu không có lỗi gì thì set giá
+            System.out.println("Đã đặt giá khởi điểm cho " + item.getName() + " là: " + newPrice);
+        }catch(Exception e){
+            System.out.println("[Lỗi]: " + e.getMessage());
+            throw e;
         }
-        auction.setAuctionSchedule(startTime);
-        System.out.println("Trạng thái hiện tại: " + auction.getStatus()); // có thì in trạng thái theo quy định hàm ở class Auction rồi
+    }
+    public void scheduleAuction(int auctionId, int itemId, LocalDateTime startTime) throws Exception{  // hàm cài đặt thời gian
+        try{
+            Items item = itemService.findItem(itemId);                           // lấy 1 sản phẩm trong kho sản phẩm theo ID
+            Auction newAuction = new Auction(auctionId, item);                   // tạo 1 phiên đấu giá
+            newAuction.setAuctionSchedule(startTime);                            // set thời gian đấu giá
+            newAuction.setStatus("PENDING");                                     // set thời gian ban đầu: cố định ở lớp Auction
+            auctionList.put(auctionId, newAuction);                              // set xong rồi thì cho lại vào danh sách các phiên đấu giá
+            System.out.println("Đã lên lịch đấu giá cho " + item.getName() + " vào lúc " + startTime);
+        }catch(Exception e){
+            System.out.println("[Lỗi]: " + e.getMessage());
+            throw e;
+        }
+    }
+    public void activateAuction(int auctionId) throws Exception{                // hàm chuyển trạng thái
+        try{
+            Auction auction = auctionList.get(auctionId);                       // lấy 1 phiên đấu giá trong danh sách các phiên đấu giá
+            LocalDateTime now = LocalDateTime.now();
+            if (auction == null) {
+                throw new Exception("Phiên đấu giá không tồn tại!");
+            }
+            if ("PENDING".equals(auction.getStatus())) {                        // check xem phiên đấu giá có trạng thái ban đầu không
+                if (now.isAfter(auction.getStartTime())) {                      // check xem thời gian đã bắt đầu chưa
+                    auction.setStatus("RUNNING");
+                    System.out.println(" Phiên đấu giá số " + auctionId + " đã chính thức BẮT ĐẦU!");
+                } else {
+                    System.out.println("Chưa đến giờ bắt đầu. Vui lòng đợi đến: " + auction.getStartTime());
+                }
+            } else {
+                System.out.println("Phiên đấu giá này không ở trạng thái PENDING nên không thể bắt đầu.");
+            }
+        }catch(Exception e){
+            System.out.println("[Lỗi]: " + e.getMessage());
+            throw e;
+        }
     }
 }
