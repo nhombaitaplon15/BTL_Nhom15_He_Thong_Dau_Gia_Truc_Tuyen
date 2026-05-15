9ae327d84f963ceb08c882858340b5f7d9eddebd
Thuy Duong <nguyentam06197205@gmail.com>
2026-05-15 23:29:37 +0700

    thêm 2 hàm test

--- src/main/java/com/auction/client/controller/ItemCardController.java
+++ src/main/java/com/auction/client/controller/ItemCardController.java
@@ -2,10 +2,12 @@ package com.auction.client.controller;
 
 import com.auction.common.model.Items;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import javafx.event.ActionEvent;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,8 +15,11 @@ public class ItemCardController {
     @FXML private ImageView imgItem;
     @FXML private Label name;
     @FXML private Label startPrice;
+    @FXML private Label timeRemaining;
+    @FXML private Button btnBidNow;
     private static Map<Integer, Items> itemsList = new HashMap<>();
     public void setData(Items item) {
+        this.currentItem = item; // Quan trọng để nút bấm biết dùng item nào
         name.setText("Tên sản phẩm: " + item.getName());
         startPrice.setText("Giá khởi điểm: " + item.getStartPrice() + " UETệ");
         try {
@@ -24,4 +29,13 @@ public class ItemCardController {
             System.out.println("Không tìm thấy ảnh cho: " + item.getName());
         }
     }
+    private Items currentItem; // Biến để lưu sản phẩm hiện tại của thẻ này
+    @FXML
+    void handleBidNow(ActionEvent event) {
+        if (currentItem != null) {
+            System.out.println("Đang thực hiện đấu giá cho: " + currentItem.getName());
+            // Tại đây bạn có thể mở màn hình chi tiết hoặc hiện popup nhập giá tiền
+        }
+    }
+
 }
